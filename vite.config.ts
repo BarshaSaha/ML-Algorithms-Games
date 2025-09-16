60379a26673e0071c3d1490d43a1fb293cb6b7bd
BarshaSaha <36075096+BarshaSaha@users.noreply.github.com>
2025-09-16 21:57:08 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Use root path on Vercel:
-  base: "/", // or simply remove the base line entirely (default is "/")
-})
