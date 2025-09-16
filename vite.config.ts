b76b96847796076d0431317aedc6fb8339e9f3e6
BarshaSaha <36075096+BarshaSaha@users.noreply.github.com>
2025-09-16 21:47:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ML-Algorithms-Games/", // <<— repo name (case-sensitive)
+  // Use root path on Vercel:
+  base: "/", // or simply remove the base line entirely (default is "/")
 })
